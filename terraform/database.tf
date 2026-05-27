7a7ccb3f2cf854d5cf012ddfbd6d3c1808a49b44
David <daviduwu123@gmail.com>
2026-05-26 18:28:22 -0600

    chore: remove automated backup retention configuration from RDS instance

--- terraform/database.tf
+++ terraform/database.tf
@@ -13,9 +13,6 @@ resource "aws_db_instance" "kuro_postgres" {
   publicly_accessible    = false
   db_subnet_group_name   = aws_db_subnet_group.kuro_db_subnet_group.name
 
-  # Backups automáticos diarios con retención de 7 días.
-  backup_retention_period = 7
-
   vpc_security_group_ids = [aws_security_group.seguridad_kuro.id]
 
   tags = {
