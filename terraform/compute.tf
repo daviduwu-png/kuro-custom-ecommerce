28ebe71b410c64e47b1522f31dc05d62d170822e
David <daviduwu123@gmail.com>
2026-05-26 19:11:46 -0600

    refactor: change instance on control plane

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "kuro_control_plane" {
   ami           = "ami-04b70fa74e45c3917"
-  instance_type = "m7i-flex.large"
+  instance_type = "t3.medium"
   subnet_id     = aws_subnet.public_a.id  # us-east-1a
 
   key_name = var.ec2_key_name
